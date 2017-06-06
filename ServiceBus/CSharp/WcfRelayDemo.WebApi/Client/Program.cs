f0c3274719720472e16f5fad8f8b71e7750dffa0
Yupeng Li <allenhula@gmail.com>
2017-06-06 12:04:26 +0800

    add sample for hosting service bus exposed wcf with IIS

--- ServiceBus/CSharp/WcfRelayDemo.WebApi/Client/Program.cs
+++ ServiceBus/CSharp/WcfRelayDemo.WebApi/Client/Program.cs
@@ -17,7 +17,7 @@ namespace Client
             // TODO: update the values
             var serviceBusNamespace = "your service bus namespace";
             var relayServicePath = "data"; // you can define your own value
-            var keyName = "RootManageSharedAccessKey";
+            var keyName = "key with sender permission";
             var key = "your key";
 
             var resourceUriString = $"https://{serviceBusNamespace}.servicebus.chinacloudapi.cn/{relayServicePath}";
@@ -53,12 +53,18 @@ namespace Client
         private static string createToken(string resourceUri, string keyName, string key)
         {
             TimeSpan sinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            // expiration one week
             var week = 60 * 60 * 24 * 7;
             var expiry = Convert.ToString((int)sinceEpoch.TotalSeconds + week);
             string stringToSign = HttpUtility.UrlEncode(resourceUri) + "\n" + expiry;
             HMACSHA256 hmac = new HMACSHA256(Encoding.UTF8.GetBytes(key));
             var signature = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(stringToSign)));
-            var sasToken = String.Format(CultureInfo.InvariantCulture, "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}", HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), expiry, keyName);
+            var sasToken = String.Format(CultureInfo.InvariantCulture, 
+                "SharedAccessSignature sr={0}&sig={1}&se={2}&skn={3}", 
+                HttpUtility.UrlEncode(resourceUri), 
+                HttpUtility.UrlEncode(signature), 
+                expiry, 
+                keyName);
             return sasToken;
         }
     }
---
+++ ServiceBus/CSharp/WcfRelayDemo.WebApi/WebWcfService/WebDataService.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class WebDataService : IDataService
+    {
+        const string CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        const int LENGTH = 10;
+
+        public string GetRandomString()
+        {
+            var random = new Random();
+            return new string(Enumerable.Repeat(CHARS, LENGTH).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+    }
+}
