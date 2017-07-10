37375c11c61a4f55d22eefe0f7e75e0e74346e5c
Yupeng Li <allenhula@gmail.com>
2017-07-10 19:39:39 +0800

    hybrid connection relay .net simple sample

---
+++ Relay/CSharp/Simple/OnPremService/OnPremService/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace OnPremService
+{
+    using System;
+    using System.IO;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Relay;
+
+    public class Program
+    {
+        // e.g. "Endpoint=sb://{RelayNamespace}.servicebus.chinacloudapi.cn/;SharedAccessKeyName={SASListenerKeyName};SharedAccessKey={SASListenerKey};EntityPath={HybridConnectionName}";
+        private const string ConnectionString = "connection string with listen permission";
+
+        public static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync()
+        {
+            var cts = new CancellationTokenSource();
+
+            var listener = new HybridConnectionListener(ConnectionString);
+
+            // Subscribe to the status events
+            listener.Connecting += (o, e) => { Console.WriteLine("Connecting"); };
+            listener.Offline += (o, e) => { Console.WriteLine("Offline"); };
+            listener.Online += (o, e) => { Console.WriteLine("Online"); };
+
+            // Opening the listener will establish the control channel to
+            // the Azure Relay service. The control channel will be continuously 
+            // maintained and reestablished when connectivity is disrupted.
+            await listener.OpenAsync(cts.Token);
+            Console.WriteLine("Server listening");
+
+            // Providing callback for cancellation token that will close the listener.
+            cts.Token.Register(() => listener.CloseAsync(CancellationToken.None));
+
+            // Start a new thread that will continuously read the console.
+            new Task(() => Console.In.ReadLineAsync().ContinueWith((s) => { cts.Cancel(); })).Start();
+
+            // Accept the next available, pending connection request. 
+            // Shutting down the listener will allow a clean exit with 
+            // this method returning null
+            while (true)
+            {
+                var relayConnection = await listener.AcceptConnectionAsync();
+                if (relayConnection == null)
+                {
+                    break;
+                }
+
+                ProcessMessagesOnConnection(relayConnection, cts);
+            }
+
+            // Close the listener after we exit the processing loop
+            await listener.CloseAsync(cts.Token);
+        }
+
+        private static async void ProcessMessagesOnConnection(HybridConnectionStream relayConnection, CancellationTokenSource cts)
+        {
+            Console.WriteLine("New session");
+
+            // The connection is a fully bidrectional stream. 
+            // We put a stream reader and a stream writer over it 
+            // which allows us to read UTF-8 text that comes from 
+            // the sender and to write text replies back.
+            var reader = new StreamReader(relayConnection);
+            var writer = new StreamWriter(relayConnection) { AutoFlush = true };
+            while (!cts.IsCancellationRequested)
+            {
+                try
+                {
+                    // Read a line of input until a newline is encountered
+                    var line = await reader.ReadLineAsync();
+
+                    if (string.IsNullOrEmpty(line))
+                    {
+                        // If there's no input data, we will signal that 
+                        // we will no longer send data on this connection
+                        // and then break out of the processing loop.
+                        await relayConnection.ShutdownAsync(cts.Token);
+                        break;
+                    }
+
+                    // Output the line on the console
+                    Console.WriteLine(line);
+
+                    // Write the line back to the client, prepending "Echo:"
+                    await writer.WriteLineAsync($"Echo: {line}");
+                }
+                catch (IOException)
+                {
+                    // Catch an IO exception that is likely caused because
+                    // the client disconnected.
+                    Console.WriteLine("Client closed connection");
+                    break;
+                }
+            }
+
+            Console.WriteLine("End session");
+
+            // Closing the connection
+            await relayConnection.CloseAsync(cts.Token);
+        }
+    }
+}
---
+++ Relay/CSharp/Simple/OutsideClient/OutsideClient/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Relay;
+
+namespace OutsideClient
+{
+    class Program
+    {
+        // e.g. "Endpoint=sb://{RelayNamespace}.servicebus.chinacloudapi.cn/;SharedAccessKeyName={SASSenderKeyName};SharedAccessKey={SASSenderKey};EntityPath={HybridConnectionName}";
+        private const string ConnectionString = "connection string with send permission";
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsync()
+        {
+            Console.WriteLine("Enter lines of text to send to the server with ENTER");
+
+            // Create a new hybrid connection client
+            var client = new HybridConnectionClient(ConnectionString);
+
+            // Initiate the connection
+            var relayConnection = await client.CreateConnectionAsync();
+
+            // We run two conucrrent loops on the connection. One 
+            // reads input from the console and writes it to the connection 
+            // with a stream writer. The other reads lines of input from the 
+            // connection with a stream reader and writes them to the console. 
+            // Entering a blank line will shut down the write task after 
+            // sending it to the server. The server will then cleanly shut down
+            // the connection which will terminate the read task.
+
+            var reads = Task.Run(async () => {
+                // Initialize the stream reader over the connection
+                var reader = new StreamReader(relayConnection);
+                var writer = Console.Out;
+                do
+                {
+                    // Read a full line of UTF-8 text up to newline
+                    string line = await reader.ReadLineAsync();
+                    // If the string is empty or null, we are done.
+                    if (String.IsNullOrEmpty(line))
+                        break;
+                    // Write to the console
+                    await writer.WriteLineAsync(line);
+                }
+                while (true);
+            });
+
+            // Read from the console and write to the hybrid connection
+            var writes = Task.Run(async () => {
+                var reader = Console.In;
+                var writer = new StreamWriter(relayConnection) { AutoFlush = true };
+                do
+                {
+                    // Read a line form the console
+                    string line = await reader.ReadLineAsync();
+                    // Write the line out, also when it's empty
+                    await writer.WriteLineAsync(line);
+                    // Quit when the line was empty
+                    if (String.IsNullOrEmpty(line))
+                        break;
+                }
+                while (true);
+            });
+
+            // Wait for both tasks to complete
+            await Task.WhenAll(reads, writes);
+            await relayConnection.CloseAsync(CancellationToken.None);
+        }
+    }
+}
