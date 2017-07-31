62e0dcb615ebab20bf08fb92684b35f0029e90f0
Yupeng Li <allenhula@gmail.com>
2017-07-31 13:31:21 +0800

    Initial version web app
    Initial version before integrated with AAD

---
+++ AAD/CSharp/AllenlAadSsoDemo-Initial/AllenlAadSsoDemo/App_Start/Startup.Auth.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AllenlAadSsoDemo
+{
+    public partial class Startup
+    {
+        public static string clientId = "";
+        public static string authority = "";
+        public static string postLogoutRedirectUri = "";
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = clientId,
+                    Authority = authority,
+                    PostLogoutRedirectUri = postLogoutRedirectUri,
+                });
+        }
+    }
+}
